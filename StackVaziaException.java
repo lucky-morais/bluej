e4a0ca952a57df5edd39a54401da5f3c0c7844fc
lucky-morais <lucky_morais@hotmail.com>
2019-07-12 14:58:50 +0100

    Create  StackVaziaException.java

---
+++ StackVaziaException.java
@@ -0,0 +1,10 @@
+public class StackVaziaException extends Exception {
+  
+  public  StackVaziaException () {
+    super();
+  }
+  
+  public StackVaziaException (String s) {
+    super(s);
+  }  
+}
